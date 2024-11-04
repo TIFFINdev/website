836be9402c6af8084898ffb8849fd821e05f7ba1
koshik8 <koshik86sk@gmail.com>
2024-11-04 19:55:13 +0530

    Update Footer.jsx (1)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-20 bg-black flex items-center justify-center">
-      <></>
+    <footer className="w-full bg-black h-20 border-2 border-gray-400 flex items-center justify-center">
+      <div className="text-white" > footer </div>
     </footer>
   );
 }
