314b59377dc824b9b92b5764ffc480a1e6e32a6d
koshik8 <koshik86sk@gmail.com>
2025-01-17 20:55:26 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import BringToCity from './components/BringToCity';
-import Download from './components/Download';
-import Footer from './components/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import BringToCity from "./components/BringToCity";
+import Download from "./components/Download";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Navbar />
-      <Hero />
+      <div className="h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-1">
+          <Hero />
+        </div>
+      </div>
       <BringToCity />
       <Download />
       <Footer />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,24 +6,30 @@ const Hero = () => {
   const [deliveryType, setDeliveryType] = useState("delivery");
 
   return (
-    <div className="flex items-end justify-between ">
-      <div className="relative w-1/2">
-        <img src="/VisualImage.png" alt="Visual Sample" className="" />
+    <div className="flex  h-full items-end justify-between ">
+      <div className="relative h-full w-1/2">
+        <img
+          src="/VisualImage.png"
+          alt="Visual Sample"
+          className="w-full absolute "
+        />
       </div>
 
-      <div className="w-1/2 px-10">
-        <h1 className="text-7xl text-right font-bold mb-8">
-          Customize your
-          <br /> own Diet
-        </h1>
-        <p className="text-gray-500 text-xl text-right font-bold mb-12">
-          Tiffin Your AI-Powered Meal solution
-        </p>
+      <div className="w-1/2  flex flex-col items-stretch justify-center space-y-24">
+        <div>
+          <h1 className="text-[5rem] pr-10 text-right font-bold mb-8">
+            Customize your
+            <br /> own Diet
+          </h1>
+          <p className="text-gray-500 pr-10 text-2xl text-right font-bold mb-12">
+            Tiffin Your AI-Powered Meal solution
+          </p>
+        </div>
 
-        <div className="bg-gray-200 rounded-2xl p-6 shadow-lg mb-16">
-          <div className="flex text-2xl mb-8 mt-4 justify-center">
+        <div className="bg-gray-200 mr-10 rounded-2xl p-6 shadow-lg mb-16">
+          <div className="flex text-2xl mb-12 mt-4 justify-center">
             <button
-              className={`px-14 py-2 rounded-lg ${
+              className={`px-14 py-3 rounded-lg ${
                 deliveryType === "delivery"
                   ? "bg-purple-100 text-purple-700"
                   : "bg-gray-100"
@@ -33,7 +39,7 @@ const Hero = () => {
               🛵 Delivery
             </button>
             <button
-              className={`px-14 py-2 rounded-lg ${
+              className={`px-14 py-3 rounded-lg ${
                 deliveryType === "pickup"
                   ? "bg-purple-100 text-purple-700"
                   : "bg-gray-100"
@@ -58,9 +64,9 @@ const Hero = () => {
             </button>
           </div>
         </div>
-
-        <div className="mt-8 text-xl bg-yellow-200 rounded-tl-full justify-end  flex items-stretch ml-0 ">
-          <div className=" px-5 py-5 flex h-auto flex-col items-center justify-center ">
+        <div className="mt-8 h-40 min-w-full text-2xl  rounded-tl-full  "></div>
+        <div className="mt-8 absolute bottom-[-17px] right-0 w-2/3 text-2xl bg-yellow-200 rounded-tl-full justify-end  flex items-stretch ml-0 ">
+          <div className="  px-4 py-12 flex h-auto flex-col items-center justify-center ">
             <p className="mb-1  text-center text-black font-medium">
               Please wait
             </p>
@@ -68,13 +74,13 @@ const Hero = () => {
               Cooking Takes Time!!
             </p>
           </div>
-          <div className="flex  rounded-l-[36px]  items-center w-2/3 bg-yellow-400 justify-center gap-8 text-black  ml-6">
+          <div className="flex  rounded-l-[36px]  items-center w-2/3 bg-yellow-400 justify-center gap-8 text-black  ml-2 py-7">
             <TimeBox label="days" value="27" />
-            <span className="text-2xl">:</span>
+            <span className="text-4xl">:</span>
             <TimeBox label="hours" value="14" />
-            <span className="text-2xl">:</span>
+            <span className="text-4xl">:</span>
             <TimeBox label="minutes" value="09" />
-            <span className="text-2xl">:</span>
+            <span className="text-4xl">:</span>
             <TimeBox label="seconds" value="59" />
           </div>
         </div>
@@ -85,8 +91,8 @@ const Hero = () => {
 
 const TimeBox = ({ label, value }) => (
   <div className="flex flex-col items-center">
-    <span className="text-4xl">{value}</span>
-    <span className="text-sm">{label}</span>
+    <span className="text-6xl">{value}</span>
+    <span className="text-lg">{label}</span>
   </div>
 );
 
